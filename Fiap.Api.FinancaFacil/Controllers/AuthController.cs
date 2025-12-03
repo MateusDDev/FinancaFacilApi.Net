e0359d7a1d6ef1d04611c55e81800ddb2e27516e
MateusDDev <ms4811978@gmail.com>
2025-12-02 22:28:16 -0300

    Format and structure improvements in `UsuarioController` and import fix in `AuthController`.

--- Fiap.Api.FinancaFacil/Controllers/AuthController.cs
+++ Fiap.Api.FinancaFacil/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Fiap.Api.FinancaFacil.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fiap.Api.FinancaFacil.Controllers
 {
--- Fiap.Api.FinancaFacil/Controllers/UsuarioController.cs
+++ Fiap.Api.FinancaFacil/Controllers/UsuarioController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Fiap.Api.FinancaFacil.Controllers;
 
-[Authorize] 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class UsuarioController : ControllerBase
@@ -20,22 +20,22 @@ public class UsuarioController : ControllerBase
         _service = exampleService;
         _mapper = mapper;
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<ListUsuariosViewModel>> List(
         [FromQuery] int cursor = 0,
         [FromQuery] int size = 10)
     {
         var serviceExamples = await _service.GetUsuarios(cursor, size);
-        
+
         var hasMore = serviceExamples.Count > size;
-        
+
         var examples = hasMore ? serviceExamples.Take(size).ToList() : serviceExamples;
-        
+
         int? nextCursor = examples.Count == size
             ? examples.Last().IdUsuario
             : null;
-        
+
         var viewModelList = _mapper.Map<IList<UsuarioViewModel>>(examples);
 
         var viewModel = new ListUsuariosViewModel
@@ -47,58 +47,56 @@ public class UsuarioController : ControllerBase
 
         return Ok(viewModel);
     }
-    
+
     [HttpGet("{id}")]
     public async Task<ActionResult<UsuarioViewModel>> Get(int id)
     {
         var usuario = await _service.GetUsuarioById(id);
-        
+
         if (usuario is null)
             return NotFound();
-        
+
         var viewModel = _mapper.Map<UsuarioViewModel>(usuario);
-        
+
         return Ok(viewModel);
     }
-    
+
     [HttpPost]
     public async Task<ActionResult> Post([FromBody] InputUsuarioViewModel viewModel)
     {
         var usuario = _mapper.Map<UsuarioModel>(viewModel);
-        
+
         await _service.CreateUsuario(usuario);
-        
+
         return CreatedAtAction(nameof(Get), new { id = usuario.IdUsuario }, viewModel);
     }
-    
- [HttpPut("{id}")]
-public async Task<ActionResult> Put(int id, [FromBody] InputUsuarioViewModel viewModel)
-{
-    var usuario = await _service.GetUsuarioById(id);
 
-    if (usuario is null)
-        return NotFound();
-
-
-    var senhaAntiga = usuario.Senha;
-
-
-    _mapper.Map(viewModel, usuario);
-
-
-    if (string.IsNullOrWhiteSpace(viewModel.Senha))
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id, [FromBody] InputUsuarioViewModel viewModel)
     {
-        usuario.Senha = senhaAntiga; 
+        var usuario = await _service.GetUsuarioById(id);
+
+        if (usuario is null)
+            return NotFound();
+
+        var senhaAntiga = usuario.Senha;
+
+        _mapper.Map(viewModel, usuario);
+        
+        if (string.IsNullOrWhiteSpace(viewModel.Senha))
+        {
+            usuario.Senha = senhaAntiga;
+        }
+        else
+        {
+            usuario.Senha = BCrypt.Net.BCrypt.HashPassword(viewModel.Senha);
+
+            await _service.UpdateUsuario(usuario);
+        }
+
+        return NoContent();
     }
-    else
-    {
-        usuario.Senha = BCrypt.Net.BCrypt.HashPassword(viewModel.Senha); 
-
-    await _service.UpdateUsuario(usuario);
-
-    return NoContent();
-}
-
+    
     [HttpDelete("{id}")]
     public async Task<ActionResult> Delete(int id)
     {
