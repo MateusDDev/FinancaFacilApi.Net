f553a415805180324888f29ca1569b8436262954
MateusDDev <ms4811978@gmail.com>
2026-04-21 21:11:44 -0300

    Convert `DicaModel` namespace declaration to file-scoped format

--- Fiap.Api.FinancaFacil/Models/DicaModel.cs
+++ Fiap.Api.FinancaFacil/Models/DicaModel.cs
@@ -1,9 +1,8 @@
-namespace Fiap.Api.FinancaFacil.Models
+namespace Fiap.Api.FinancaFacil.Models;
+
+public class DicaModel
 {
-    public class DicaModel
-    {
-        public int IdDica { get; set; }
-        public string Texto { get; set; } = string.Empty;
-        public string Categoria { get; set; } = string.Empty;
-    }
+    public int IdDica { get; set; }
+    public string Texto { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
 }
